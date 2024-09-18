b99e09748b4cd915f9faea3dd7f5caecad2267af
ignismeow <saadbeenco@gmail.com>
2024-09-18 10:37:27 +0500

    updated bit htm, css and javascript

---
+++ README.md
@@ -0,0 +1,15 @@
+
+```
+menu-ordering-app
+├─ images
+│  ├─ Beer.png
+│  ├─ burger.png
+│  ├─ headerbanner.png
+│  └─ pizza.png
+├─ index.css
+├─ index.html
+└─ scripts
+   ├─ data.js
+   └─ index.js
+
+```
--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,31 @@
-import { menuArray } from '../data';
+import menuArray from './data.js';  // Import menuArray from data.js
 
-const menuEl = document.getElementById('main-container');
+const menuEl = document.getElementById('menu-container');
+const addButton = document.getElementById('add-button')
 
-console.log(menuEl)
-console.log(menuArray)
+const menu = menuArray.map(item => {
+    return ` <div class="menu-item">
+                <div class="menu-item-details">
+                    <div class="menu-item-image">
+                        <img src="${item.image}">
+                    </div>
+                    <div class="menu-item-text">
+                        <h2>${item.name}</h2>
+                        <p>${item.ingredients}</p>
+                        <p id="price">$${item.price}</p>
+                    </div>
+                </div>
+                <div class="menu-item-button">
+                    <button id="add-button">
+                        <i class="fa-solid fa-plus"></i>
+                    </button>
+                </div>
+            </div>`
+}).join('')
 
+menuEl.innerHTML = menu
+
+
+addButton.addEventListener('click', function(){
+    console.log('button clicked')
+})
