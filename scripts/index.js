eb139992b59728b713cee35762db4936b206ef2b
ignismeow <saadbeenco@gmail.com>
2024-09-19 11:31:45 +0500

    "Updated CSS selectors, HTML structure, and JavaScript event listeners for menu ordering functionality."

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,9 @@ import menuArray from './data.js';  // Import menuArray from data.js
 
 const menuEl = document.getElementById('menu-container');
 const addButton = document.getElementById('add-button')
+const yourOrderEl = document.getElementById('your-order')
+const orderContainer = document.getElementById('order-container')
+const buttons = document.querySelectorAll('button');
 
 const menu = menuArray.map(item => {
     return ` <div class="menu-item">
@@ -16,7 +19,7 @@ const menu = menuArray.map(item => {
                     </div>
                 </div>
                 <div class="menu-item-button">
-                    <button id="add-button">
+                    <button class="add-button" id="add-button" data-order-btn="${item.name}">
                         <i class="fa-solid fa-plus"></i>
                     </button>
                 </div>
@@ -25,7 +28,54 @@ const menu = menuArray.map(item => {
 
 menuEl.innerHTML = menu
 
+document.addEventListener('click', function(e) {
+   if(e.target.dataset.orderBtn){
+    addToMenu(e.target.dataset.orderBtn)
+   }
+});
 
-addButton.addEventListener('click', function(){
-    console.log('button clicked')
-})
+
+function addToMenu(clickedItem) {
+    const orderHtml = ` 
+                <div class="ordered-list" id="ordered-list">
+                    <div>
+                        <p>${item.name}</p>
+                        <button class="remove-btn">remove</button>
+                    </div>
+                    <div>
+                        <p>${item.price}</p>
+                    </div>
+                </div>
+                <div class="ordered-list" id="ordered-list">
+                    <div>
+                        <p>Total price:</p>
+                    </div>
+                    <div>
+                        <p>$30.99</p>
+                    </div>
+                </div>      
+            `
+    menuArray.forEach(item =>{
+        if(item.name === clickedItem){
+            yourOrderEl.innerHTML = orderHtml
+        }
+    })
+}
+
+{/* <div class="ordered-list" id="ordered-list">
+                    <div>
+                        <p>${item.name}</p>
+                        <button class="remove-btn">remove</button>
+                    </div>
+                    <div>
+                        <p>${item.price}</p>
+                    </div>
+                </div>
+                <div class="ordered-list" id="ordered-list">
+                    <div>
+                        <p>Total price:</p>
+                    </div>
+                    <div>
+                        <p>$30.99</p>
+                    </div>
+                </div> */}
